2113875832cc4d483f89e440dbba305163be2041
dvcarrot <dvcarrot@yandex.ru>
2019-01-24 22:52:10 +0300

    extract vendor js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
+const pkg = require('./package.json');
+const dependencies = Object.keys(pkg.dependencies);
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
+gulp.task('vendor', function () {
+    const browserify = require('browserify');
+    const vinylSourceStream = require('vinyl-source-stream');
+    const vinylBuffer = require('vinyl-buffer');
+    return browserify()
+        .require(dependencies)
+        .bundle()
+        .pipe(vinylSourceStream('vendor.js'))
+        .pipe(vinylBuffer())
+        .pipe($.uglify())
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('scripts', function () {
     return gulp.src('src/*.js')
-        .pipe($.browserify())
+        .pipe($.browserify({basedir: '.', external: dependencies}))
         .pipe($.uglify())
         .pipe(gulp.dest('dist'))
 });
@@ -13,4 +28,4 @@ gulp.task('views', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', gulp.parallel('scripts', 'views'));
+gulp.task('build', gulp.parallel('vendor', 'scripts', 'views'));
